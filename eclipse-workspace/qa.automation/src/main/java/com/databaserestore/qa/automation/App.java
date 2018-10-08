927085d1681bf8947101828f65ec7c28ba4e97d2
Ramesh <gvramesh2@gmail.com>
2018-10-08 11:50:20 +0530

    adding new files to the project

---
+++ eclipse-workspace/qa.automation/src/main/java/com/databaserestore/qa/automation/App.java
@@ -0,0 +1,13 @@
+package com.databaserestore.qa.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
